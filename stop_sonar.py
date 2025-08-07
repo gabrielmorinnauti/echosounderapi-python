768a67aba966bfbdde421935add1c17bae3e6ea8
curieuxmorin <216788021+gabrielmorinnauti@users.noreply.github.com>
2025-08-07 13:38:48 -0400

    Create stop_sonar.py

---
+++ stop_sonar.py
@@ -0,0 +1,22 @@
+import re
+import csv
+import time
+from echosndr import DualEchosounder
+
+''' Configuration '''
+PORT = "COM10"
+BAUD = 115200
+
+''' Sonar Connection & Update Parameters'''
+try:
+    sonar = DualEchosounder(PORT, BAUD)
+except:
+    print("❌ Port non accessible")
+    exit()
+
+if not sonar.IsDetected():
+    print("❗ Sonar non détecté")
+    exit()
+
+sonar.Stop()
+print("✅ Sonar détecté")
